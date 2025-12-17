6333cc867aee68ae5537d84af909f0c2590b9463
Somesh Joshi <joshi.somesh1996@gmail.com>
2025-12-17 18:41:51 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Somesh Joshi",
+    author_email="joshi.somesh1996@gmail.com",
+    packages=find_packages()
+)
